1d5a0140c3cdc62ec2ac7c8c0b5ebbabaf633012
Jo <whtprl7777@gmail.com>
2022-11-24 01:07:33 +0900

    ver 1.00

---
+++ ___Keys___.py
@@ -0,0 +1,24 @@
+import keyboard
+import time
+
+allowed_key_list = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                    "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12",
+                    "`", "-", "+", "=", "/", "*", "decimal", "<", ">", "?",
+                    "[", "]", "\\", ";", "'", ",", ".", "{", "}", "|", ":", "\"", "(", ")",
+                    "!", "@", "#", "$", "%", "^", "&", "_",
+                    "print screen", "scroll lock", "pause", "delete", "end", "page down",
+                    "insert", "home", "page up",
+                    "left", "right", "up", "down",
+                    "esc", "caps lock", "shift" , "right shift", "ctrl", "right ctrl", "alt", "enter", "space",
+                    "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+                    "a", "s", "d" , "f", "g", "h", "j", "j", "k", "l",
+                    "z", "x", "c", "v", "b", "n", "m",
+                    "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
+                    "A", "S", "D", "F", "G", "H", "J", "K", "L",
+                    "Z", "X", "C", "V", "B", "N", "M")
+
+only_number_keys = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+
+toggle_key = "f12"
+
+
